2bd713ab105c40f6caba40c02ab384770eac07d2
marsal <marsal@DESKTOP-SL3HUAK>
2021-07-14 14:32:45 +0530

    Add project files.

---
+++ WebApplication26/Client/Allservices/Services.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication26.Client.Allservices
+{
+    public class SingletonServices
+    {
+        public int value { get; set; }
+
+    }
+}
+
+namespace WebApplication26.Client.Allservices
+{
+    public class TransientServices
+    {
+        public int value { get; set; }
+    }
+}
